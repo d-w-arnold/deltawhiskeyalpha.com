16134eef74e9f094b526bc20170e00ecc9817499
David Arnold <David@davidMacPro.local>
2017-09-29 23:07:22 +0100

    Added favicon(s) to website.

--- topHTML.php
+++ topHTML.php
@@ -3,6 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/resources/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/resources/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/resources/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/resources/favicon/manifest.json">
+    <link rel="mask-icon" href="/resources/favicon/safari-pinned-tab.svg" color="#282828">
+    <link rel="shortcut icon" href="/resources/favicon/favicon.ico">
+    <meta name="msapplication-config" content="/resources/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#282828">
     <meta name="description" content="∆WA : Home of Computer Scientist, David W. Arnold.">
     <title><?php echo $title ?: '∆WA : David W. Arnold'?></title>
     <link rel="stylesheet" href="stylesheet.css">
