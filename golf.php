b44adb1bc38bf1f75cd16ed89d367722ff976920
David Arnold <David@davidMacPro.local>
2017-10-13 13:31:37 +0100

    Updated golf profile

--- golf.php
+++ golf.php
@@ -18,11 +18,11 @@ include "./topHTML.php";
     <br>
     <p>In January 2015, I ventured to Newberry, South Carolina (USA) to study and play golf for the Newberry College NCAA golf team - I was scouted by their head coach, and was offered 100% sporting and academic scholarship, to both train and play for the team and to study as a Mathematics Major.</p>
     <br>
-    <p>Having returned to the UK in October 2016, I relinquished my amateur status and turned professional in January 2016 (aged 19) and joined the EuroPro UK Golf Tour for the 2016 season.</p>
+    <p>In October 2016, I relinquished my amateur status and turned professional in January 2016 (aged 19) and joined the EuroPro UK Golf Tour for the 2016 season.</p>
     <br>
     <p>During the EuroPro qualifying stages in March 2016, I was struck with illness and was limited in my ability to qualify for a high ranking position on the tour - making it difficult to enter events (due to limited spaces per event) for the remainder of the year.</p>
     <br>
-    <p>By August 2016, I had decided to finally hang up my golf clubs in order to turn my attention to an unconditional offer for undergraduate study in Computer Science at the University of Kent.</p>
+    <p>In August 2016, I decided to hang up my golf clubs professionally in order to turn my attention to an unconditional offer for undergraduate study in Computer Science at the University of Kent.</p>
     <br>
     <p>Golf remains a passion of mine, and will remain a lifetime hobby.</p>
 </div>
