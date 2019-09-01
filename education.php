929b2c11a393dada29307a31665a3184e9a1ad92
David W. Arnold <d.w.arnold.gm@gmail.com>
2019-09-01 22:07:03 +0100

    Added Red Bull work, updated skills, and reformatted entries in Education and Computing sections

--- education.php
+++ education.php
@@ -10,55 +10,111 @@ include "./topHTML.php";
 
     <p class="title">Education</p>
 
-    <p class="university"><b class="uniName">University of Kent</b>, Canterbury, U.K.</p>
-    <p class="uniCourseName">BSc Computer Science with a Year in Industry</p>
-    <p class="uniCourseDates">Sep. 2016 - Jun. 2020</p>
-    <div class="uniCourseDetails">
-        <p>•&nbsp;&nbsp;Year 1: 86%</p>
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO320: Introduction to Object-Orientated Programming (Java): 97%</p>
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO323: Database and the Web (PHP, MySQL, HTML & CSS, JavaScript): 86%</p>
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO520: Further Object-Orientated Programming (Java): 94%</p>
-    </div>
-    <div class="uniCourseDetails">
-        <p>•&nbsp;&nbsp;Year 2: 78%</p>
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO518: Algorithms , Correctness and Efficiency (Java): 80%</p>
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO539: Web Development: (HTML & CSS, JavaScript w/ jQuery, AJAX & JQueryUI, PHP): 95%</p>
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO545: Functional and Concurrent Programming (Erlang w/ Haskell): 94%</p>
-    </div>
+    <!--University of Kent-->
+    <table style="margin-bottom: 2%">
+        <tr>
+            <td class="employer"><b class="employerName"><a href="https://www.kent.ac.uk">University of Kent</a></b></td>
+            <td class="employer" style="text-align: right">Canterbury, U.K.</td>
+        </tr>
+    </table>
+
+    <table style="margin-bottom: 2%">
+        <tr><td class="jobTitle">BSc Computer Science with a Year in Industry</td></tr>
+        <tr><td class="jobDates">Sep. 2016 - Jun. 2020</td></tr>
+    </table>
+
+    <table>
+        <tr>
+            <div class="uniCourseDetails">
+                <p>•&nbsp;&nbsp;Year 1: 86%</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO320: Introduction to Object-Orientated Programming (Java): 97%</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO323: Database and the Web (PHP, MySQL, HTML & CSS, JavaScript): 86%</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO520: Further Object-Orientated Programming (Java): 94%</p>
+            </div>
+            <div class="uniCourseDetails">
+                <p>•&nbsp;&nbsp;Year 2: 78%</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO518: Algorithms , Correctness and Efficiency (Java): 80%</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO539: Web Development: (HTML & CSS, JavaScript w/ jQuery, AJAX & JQueryUI, PHP): 95%</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;CO545: Functional and Concurrent Programming (Erlang w/ Haskell): 94%</p>
+            </div>
+        </tr>
+    </table>
 
     <div class="spacing"></div>
 
-    <p class="university"><b class="uniName">Newberry College</b>, Newberry South Carolina, U.S.A.</p>
-    <p class="uniCourseName">Mathematics Major</p>
-    <p class="uniCourseDates">Jan. 2015 - Oct. 2015</p>
-    <div class="uniCourseDetails">
-        <p>•&nbsp;&nbsp;I was offered a 100% scholarship (golf and academic) to play for the Newberry College NCAA (National Collegiate Athletic Association) golf team while studying as a Mathematics Major.</p>
-        <p>•&nbsp;&nbsp;My GPA (Grade Point Average) for the time I was there was 3.80/4.00 (A-Grade).</p>
-        <p>•&nbsp;&nbsp;I returned to the U.K. to pursue a degree at a U.K. university.</p>
-    </div>
+    <!--Newberry College-->
+    <table style="margin-bottom: 2%">
+        <tr>
+            <td class="employer"><b class="employerName"><a href="https://www.newberry.edu">Newberry College</a></b></td>
+            <td class="employer" style="text-align: right">Newberry, South Carolina, U.S.A.</td>
+        </tr>
+    </table>
+
+    <table style="margin-bottom: 2%">
+        <tr><td class="jobTitle">Mathematics Major</td></tr>
+        <tr><td class="jobDates">Jan. 2015 - Oct. 2015</td></tr>
+    </table>
+
+    <table>
+        <tr>
+            <div class="uniCourseDetails">
+                <p>•&nbsp;&nbsp;I was offered a 100% scholarship (golf and academic) to play for the Newberry College NCAA (National Collegiate Athletic Association) golf team while studying as a Mathematics Major.</p>
+                <p>•&nbsp;&nbsp;My GPA (Grade Point Average) for the time I was there was 3.80/4.00 (A-Grade).</p>
+                <p>•&nbsp;&nbsp;I returned to the U.K. to pursue a degree at a U.K. university.</p>
+            </div>
+        </tr>
+    </table>
 
     <div class="spacing"></div>
 
-    <p class="university"><b class="uniName">The Open University</b>, Milton Keynes, U.K.</p>
-    <p class="uniCourseName">Certificate of Higher Education (Open)</p>
-    <p class="uniCourseDates">Jun. 2012 - Jul. 2014</p>
-    <div class="uniCourseDetails">
-        <p>•&nbsp;&nbsp;I studied (part-time) for two years: Computing, Psychology and Mathematics.</p>
-        <p>•&nbsp;&nbsp;Computing:</p>
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;Overall Continuous Assessment Score: 75%</p>
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;Overall Examinable Score: 89%</p>
-    </div>
+    <!--The Open University-->
+    <table style="margin-bottom: 2%">
+        <tr>
+            <td class="employer"><b class="employerName"><a href="http://www.open.ac.uk">The Open University</a></b></td>
+            <td class="employer" style="text-align: right">Milton Keynes, U.K.</td>
+        </tr>
+    </table>
+
+    <table style="margin-bottom: 2%">
+        <tr><td class="jobTitle">Certificate of Higher Education (Open)</td></tr>
+        <tr><td class="jobDates">Jun. 2012 - Jul. 2014</td></tr>
+    </table>
+
+    <table>
+        <tr>
+            <div class="uniCourseDetails">
+                <p>•&nbsp;&nbsp;I studied (part-time) for two years: Computing, Psychology and Mathematics.</p>
+                <p>•&nbsp;&nbsp;Computing:</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;Overall Continuous Assessment Score: 75%</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;–&nbsp;&nbsp;Overall Examinable Score: 89%</p>
+            </div>
+        </tr>
+    </table>
 
     <div class="spacing"></div>
 
-    <p class="university"><b class="uniName">The Harvey Grammar School</b>, Folkestone, U.K.</p>
-    <p class="uniCourseName">GCSE (General Certificate of Higher Education)</p>
-    <p class="uniCourseDates">Sep. 2007 - Jul. 2012</p>
-    <div class="uniCourseDetails">
-        <p>•&nbsp;&nbsp;(A-Grade): Mathematics, Science, Additional Science, English, Design & Technology: Resistant Materials, BTEC Level 2 Certificate in Sport - Distinction*</p>
-        <p>•&nbsp;&nbsp;(B-Grade): Physical Education, English Literature, Geography, Media Studies</p>
-        <p>•&nbsp;&nbsp;(C-Grade): Business Studies, Statistics</p>
-    </div>
+    <!--The Harvey Grammar School-->
+    <table style="margin-bottom: 2%">
+        <tr>
+            <td class="employer"><b class="employerName"><a href="http://www.harveygs.kent.sch.uk">The Harvey Grammar School</a></b></td>
+            <td class="employer" style="text-align: right">Folkestone, U.K.</td>
+        </tr>
+    </table>
+
+    <table style="margin-bottom: 2%">
+        <tr><td class="jobTitle">GCSE (General Certificate of Higher Education)</td></tr>
+        <tr><td class="jobDates">Sep. 2007 - Jul. 2012</td></tr>
+    </table>
+
+    <table>
+        <tr>
+            <div class="uniCourseDetails">
+                <p>•&nbsp;&nbsp;(A-Grade): Mathematics, Science, Additional Science, English, Design & Technology: Resistant Materials, BTEC Level 2 Certificate in Sport - Distinction*</p>
+                <p>•&nbsp;&nbsp;(B-Grade): Physical Education, English Literature, Geography, Media Studies</p>
+                <p>•&nbsp;&nbsp;(C-Grade): Business Studies, Statistics</p>
+            </div>
+        </tr>
+    </table>
 
 </div>
 
--- golf.php
+++ golf.php
@@ -13,8 +13,6 @@ include "./topHTML.php";
     <div class="writing">
         <p>Before I became a Computer Scientist, I was a golfer. My golf career started at the age of 7. My grandfather, who lived next to Littlestone Golf Club (Kent, U.K.), bought me my first set of golf clubs for my 7th birthday. As a beginning amateur, I earned myself a handicap of 28 at the age of 8, and by the age of 19, I had brought that down to -1. Over the years I represented Kent County Golf Union in various fixtures for their various teams: Kent Under 13’s, U14’s, U16’s (Captain), U18’s.</p>
         <br>
-        <p>My golfing accolades include: Kent Schools Under 16 Champion 2011, Littlestone Junior Open Champion 2011, Kent Junior League Winning Team Member 2010, Kent U21 Scratch League Winning Team Member 2012, Kent Youth (U21) Championships 2013 - 6th place, Winner Peter Belsham Trophy (Scratch Matchplay - beating 2 England team players) 2013, Kent Men’s Amateur Championships 2014 - 9th place, Kent Men’s Scratch Matchplay (Guilford Trophy, team event) 2014 - 2nd place, South East Junior Foursomes (Kent U18 squad, Matchplay) 2014 - 2nd place, Kent Amateur Championships 2015 - Top 20.</p>
-        <br>
         <p>In January 2016, I relinquished my amateur status and turned professional (aged 19) joining the EuroPro UK Golf Tour for the 2016 season. In August 2016, I hung up my golf clubs professionally in order to turn my attention to an unconditional offer for undergraduate study in Computer Science at the University of Kent. Golf remains a passion of mine, and will remain a lifetime hobby.</p>
     </div>
 
