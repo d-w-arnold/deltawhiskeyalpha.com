92d1d83fb5138dce9159c76ba85cb23e0c0b7210
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-06-07 19:40:33 +0100

    Moved favicon files to project root directory, to enable search engine crawling of favicon.ico

--- topHTML.php
+++ topHTML.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" sizes="180x180" href="/resources/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/resources/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/resources/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/resources/favicon/manifest.json">
-    <link rel="mask-icon" href="/resources/favicon/safari-pinned-tab.svg" color="#282828">
-    <link rel="shortcut icon" href="/resources/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#282828">
+    <link rel="shortcut icon" href="/favicon/favicon.ico">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
     <link rel="stylesheet" href="./resources/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./style.css">
-    <meta name="msapplication-config" content="/resources/favicon/browserconfig.xml">
+    <meta name="msapplication-config" content="/favicon/browserconfig.xml">
     <meta name="theme-color" content="#282828">
     <meta name="description"
           content="Home of Computer Scientist and University of Kent undergraduate, David W. Arnold.">
@@ -23,7 +23,7 @@
 <?php $cv_path = '/resources/David_W_Arnold-CV-website.pdf' ?>
 <div id="loader">
     <div id="loading">
-        <img id="loading-icon" src="./resources/favicon/android-chrome-192x192.png" alt="Loading ...">
+        <img id="loading-icon" src="./favicon/android-chrome-192x192.png" alt="Loading ...">
     </div>
 </div>
 <div id="myDiv" class="animate-bottom">
