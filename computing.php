6c4ef68345a6b7bfff57b671477f6b0f16bf34ac
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-11-19 09:54:59 +0000

    Added updated after summer projects and added new job post-uni

--- computing.php
+++ computing.php
@@ -18,7 +18,7 @@ include "./topHTMLnoIndex.php";
             </tr>
             <tr>
                 <td class="computingSubTitle">Skills</td>
-                <td class="computingWriting">Unix CLI, Git, JetBrains IDEs, Java, C, C++, Python, Erlang, JavaScript (w/
+                <td class="computingWriting">Unix CLI, Git, JetBrains IDEs, Java, Python, C, C++, Erlang, JavaScript (w/
                     React, jQuery, AJAX &
                     Node.js), TypeScript, Docker, PHP, MySQL, HTML, SCSS, Shell, Rust, R,
                     AppleScript, Markdown, LaTeX, Cryptography, Mathematics
@@ -27,18 +27,6 @@ include "./topHTMLnoIndex.php";
             <tr>
                 <td class="computingSubTitle">Projects</td>
                 <td class="computingWriting">
-                    <!-- Web Dev -->
-                    <a target="_blank" href="https://github.com/d-w-arnold/dwa_website_react">My First Dockerised React
-                        Website</a> (React, TypeScript, Docker, Docker-Compose, PHP & SCSS)<br>
-                    <a target="_blank" href="https://github.com/d-w-arnold/online_test_DWP">My First OpenAPI</a> (Java,
-                    OpenAPI & Docker-Compose)<br>
-                    <a target="_blank" href="https://github.com/d-w-arnold/technical_challenge_D3.js">CV
-                        Visualisation</a>
-                    (React, D3.js, TypeScript & SCSS)<br>
-                    <a target="_blank" href="https://github.com/d-w-arnold/deltawhiskeyalpha.com">My First Website</a>
-                    (PHP, HTML, CSS & jQuery)<br>
-                    <a target="_blank" href="https://github.com/d-w-arnold/CO657-IoT_Admin_Web_App">IoT Admin Web
-                        App</a> (PHP, MySQL & CSS)<br>
                     <!-- Java -->
                     <a target="_blank" href="https://github.com/d-w-arnold/CO634-Assignment-2">Ciphertext Decryption</a>
                     (Java)<br>
@@ -46,34 +34,46 @@ include "./topHTMLnoIndex.php";
                     Maven)<br>
                     <a target="_blank" href="https://github.com/d-w-arnold/CO657-IoT_Server">IoT Server</a> (Java &
                     JDBC)<br>
-                    <a target="_blank" href="https://github.com/d-w-arnold/Fairy_Lights_Controller">Lights
-                        Controller</a> (Java & JUnit)<br>
                     <a target="_blank" href="https://github.com/d-w-arnold/CO633-Computer_Networks_Assignments">Data-Link
                         Protocol</a> (Java)<br>
+                    <a target="_blank" href="https://github.com/d-w-arnold/Fairy_Lights_Controller">Lights
+                        Controller</a> (Java & JUnit)<br>
                     <a target="_blank" href="https://github.com/d-w-arnold/Reflective-Journal_CSV-to-TXT-Converter">CSV
                         to TXT Converter</a> (Java)<br>
                     <a target="_blank" href="https://github.com/d-w-arnold/Java_Sudoku-Solver">Sudoku Solver</a> (Java &
                     JavaFX)<br>
-                    <!-- C/C++ -->
-                    <a target="_blank" href="https://github.com/d-w-arnold/Command_Line_Address_Book">Command-Line
-                        Address Book</a> (C)<br>
-                    <a target="_blank" href="https://github.com/d-w-arnold/CO657-IoT_Device">IoT BLE & Motion
-                        Detection</a> (C++)<br>
-                    <a target="_blank" href="https://github.com/d-w-arnold/C_Test_Bench">C Test Bench</a> (C)<br>
-                    <a target="_blank" href="https://github.com/d-w-arnold/online_test_PetaGene_C">Coderbyte Test</a>
-                    (C++)<br>
                     <!-- Python -->
                     <a target="_blank" href="https://github.com/d-w-arnold/local_news-data_collection">News Web
                         Crawler</a> (Python)<br>
                     <a target="_blank" href="https://github.com/d-w-arnold/Python_Sudoku-Solver">Sudoku Solver</a>
                     (Python)<br>
+                    <!-- C/C++ -->
+                    <a target="_blank" href="https://github.com/d-w-arnold/Command_Line_Address_Book">CLI Address
+                        Book</a> (C)<br>
+                    <a target="_blank" href="https://github.com/d-w-arnold/CO657-IoT_Device">IoT BLE & Motion
+                        Detection</a> (C++)<br>
                     <!-- Erlang -->
                     <a target="_blank" href="https://github.com/d-w-arnold/CO545-Assignment-4">TCP
-                        Packets</a>(Erlang)<br>
+                        Packets</a> (Erlang)<br>
                     <!-- Misc. -->
-                    <a target="_blank" href="https://github.com/d-w-arnold/Gambit_Cipher_Quiz">Cipher Quiz</a> (C++,
-                    Python & Erlang)<br>
-                    <a target="_blank" href="https://github.com/d-w-arnold/Fast_Food_Ranker">Randomiser</a> (Shell)<br>
+                    <a target="_blank" href="https://github.com/d-w-arnold/Gambit_Cipher_Quiz">Cipher Quiz</a> (Python,
+                    C++ & Erlang)<br>
+                    <!-- Web Dev -->
+                    <a target="_blank" href="https://github.com/d-w-arnold/online_test_DWP">My First OpenAPI</a> (Java,
+                    OpenAPI & Docker-Compose)<br>
+                    <a target="_blank" href="https://github.com/d-w-arnold/technical_challenge_D3.js">CV
+                        Visualisation</a> (React, D3.js, TypeScript & SCSS)<br>
+                    <a target="_blank" href="https://github.com/d-w-arnold/laravel-test">Simple Questionnaire</a> (PHP,
+                    Laravel & MySQL)<br>
+                    <a target="_blank" href="https://github.com/d-w-arnold/dwa_website_react">My First Dockerised React
+                        Website</a> (React, TypeScript, Docker, Docker-Compose, PHP & SCSS)<br>
+                    <a target="_blank" href="https://github.com/d-w-arnold/CO657-IoT_Admin_Web_App">IoT Admin Web
+                        App</a> (PHP, MySQL & CSS)<br>
+                    <a target="_blank" href="https://github.com/d-w-arnold/deltawhiskeyalpha.com">My First Website</a>
+                    (PHP, HTML, CSS & jQuery)<br>
+                    <!-- Non-CV -->
+                    <a target="_blank" href="https://github.com/d-w-arnold/Fast_Food_Ranker">Fast Food Randomiser</a>
+                    (Shell)<br>
                     <a target="_blank" href="https://github.com/d-w-arnold/AppleScript_Timestamp-Media">Timestamp
                         Media</a> (AppleScript)<br>
                 </td>
@@ -93,6 +93,26 @@ include "./topHTMLnoIndex.php";
     <div class="spacing6"></div>
 
     <p class="title">Experience</p>
+
+    <!--Peach Media-->
+    <div class="roboto">
+        <table class="marginBottom2">
+            <tr>
+                <td class="employerName"><span class="iconLap" aria-hidden="true"></span><b><a
+                                target="_blank" href="https://www.peachvideo.com/en-gb/media/">Peach Media</a></b></td>
+                <td class="location">London, U.K.</td>
+            </tr>
+        </table>
+
+        <table class="marginBottom2">
+            <tr>
+                <td class="jobTitle">Junior Software Engineer</td>
+            </tr>
+            <tr>
+                <td class="jobDates">Oct. 2020 - Current</td>
+            </tr>
+        </table>
+    </div>
 
     <!--Red Bull-->
     <div class="roboto">
@@ -143,34 +163,6 @@ include "./topHTMLnoIndex.php";
     </div>
 
     <div class="spacing2"></div>
-
-    <!--IT-Alan-->
-    <div class="roboto">
-        <table class="marginBottom2">
-            <tr>
-                <td class="employerName"><span class="iconLap" aria-hidden="true"></span><b><a
-                                target="_blank" href="https://www.it-alan.co.uk/">IT-Alan</a></b></td>
-                <td class="location">Burmarsh, Kent, U.K.</td>
-            </tr>
-        </table>
-
-        <table class="marginBottom2">
-            <tr>
-                <td class="jobTitle">Assistant Engineer</td>
-            </tr>
-            <tr>
-                <td class="jobDates">Jun. 2017 - Sep. 2017</td>
-            </tr>
-        </table>
-
-        <table>
-            <tr>
-                <td class=jobDetails>
-                    <p>•&nbsp;&nbsp;Computer repair & maintenance with a Microsoft Certified Systems Engineer.</p>
-                </td>
-            </tr>
-        </table>
-    </div>
 
 </div>
 
--- index.php
+++ index.php
@@ -17,35 +17,7 @@ include './topHTML.php';
             motivated, organised, reliable and work well both independently and as part of a team.</p>
     </div>
 
-    <div class="spacing2"></div>
-
-    <!--
-    PeoplePerHour Profile Widget
-    The div#pph-hire me is the element
-    where the iframe will be inserted.
-    You may move this element wherever
-    you need to display the widget
--->
-    <div id="pph-hireme"></div>
-    <script type="text/javascript">
-        (function (d, s) {
-            var useSSL = 'https:' == document.location.protocol;
-            var js, where = d.getElementsByTagName(s)[0],
-                js = d.createElement(s);
-            if (window.innerWidth > 600) {
-                js.src = (useSSL ? 'https:' : 'http:') + '//www.peopleperhour.com/hire/2209121290/4399933.js?width=300&height=135&orientation=vertical&theme=dark&rnd=' + parseInt(Math.random() * 10000, 10);
-            } else {
-                js.src = (useSSL ? 'https:' : 'http:') + '//www.peopleperhour.com/hire/2209121290/4399933.js?width=240&height=135&orientation=vertical&theme=dark&rnd=' + parseInt(Math.random() * 10000, 10);
-            }
-            try {
-                where.parentNode.insertBefore(js, where);
-            } catch (e) {
-                if (typeof console !== 'undefined' && console.log && e.stack) {
-                    console.log(e.stack);
-                }
-            }
-        }(document, 'script'));
-    </script>
+    <div class="spacing4"></div>
 
     <p class="title">Quantum Cryptography: Security for the Post-Quantum world</p>
 
