58e78a67740bdb9b1018df2df846b69f33fdd1af
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-04-17 18:42:43 +0100

    LinkedIn badge broken, hidden away left section on Contact page temporarily

--- contact.php
+++ contact.php
@@ -21,19 +21,9 @@ include "./topHTMLnoIndex.php";
     <div class="roboto">
         <div class="contact-details">
             <div class="left">
-                <div class="LI-profile-badge" data-version="v1" data-size="medium" data-locale="en_US"
-                     data-type="vertical"
-                     data-theme="dark" data-vanity="david-w-arnold"><a class="LI-simple-link"
-                                                                       href='https://uk.linkedin.com/in/david-w-arnold?trk=profile-badge'>David
-                        W. Arnold</a></div>
-                <div id="github" class="center logos"><a href="https://github.com/d-w-arnold" target="_blank"><img
-                                src="/resources/github_white.png"
-                                alt="GitHub"
-                                width="64" height="64"></a></div>
-                <div id="keybase" class="center logos"><a href="https://keybase.io/d_w_arnold" target="_blank"><img
-                                src="/resources/keybase-tile.svg"
-                                alt="Keybase"
-                                width="64" height="64"></a></div>
+                <!--<div class="LI-profile-badge" data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="david-w-arnold"><a class="LI-simple-link" href='https://uk.linkedin.com/in/david-w-arnold?trk=profile-badge'>David W. Arnold</a></div>-->
+                <!--<div id="github" class="center logos"><a href="https://github.com/d-w-arnold" target="_blank"><img src="/resources/github_white.png" alt="GitHub" width="64" height="64"></a></div>-->
+                <!--<div id="keybase" class="center logos"><a href="https://keybase.io/d_w_arnold" target="_blank"><img src="/resources/keybase-tile.svg" alt="Keybase" width="64" height="64"></a></div>-->
             </div>
             <div class="right">
                 <form id="contact-form" action="<?php header('Location:/contact.php#contactTitle'); ?>" method="POST">
